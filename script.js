770c641c86ec49d3b15e83ac366dd11f678f210a
ChristianHS23 <christian.sihotang23@gmail.com>
2019-01-10 21:59:15 +0700

    add TopchartTable

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ function getLyrics(artist, title) {
       console.log(err);
     })
 }
+
+function getTopChart() {
+  let i = 0
+  axios({
+    method: 'GET',
+    url: 'http://localhost:3000/topcharts'
+  })
+  .then(({data}) =>  {
+    data.track.forEach(track => {
+      i++
+      $('#topchart').append(`
+        <tr>
+          <th scope="row">${i}</th>
+          <td>${track.artist.name}</td>
+          <td>${track.name}</td>
+        </tr>`)
+    })
+    console.log(data.track);
+  })
+  .catch(err => {
+    console.log(err);
+    
+  })
+}
