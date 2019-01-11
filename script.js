253b55402187479e00de6cc3a6aea3cd320e41d5
imanuelvicky <inv484@gmail.com>
2019-01-11 10:02:45 +0700

    layout fixing

--- script.js
+++ script.js
@@ -59,3 +59,49 @@ function getVideo(find) {
     console.log(err);
   })
 }
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  // console.log('Name: ' + profile.getName());
+  // console.log('Image URL: ' + profile.getImageUrl());
+  // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  var id_token = googleUser.getAuthResponse().id_token;
+  axios({
+    method: 'POST',
+    url: 'http://localhost:3000/users/signin',
+    data: {
+      google_access_token: id_token
+    }
+  })
+  .then(({ data }) => {
+    console.log(data)
+    localStorage.setItem('token', data)
+    isLogin()
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+function isLogin() {
+  let token = localStorage.getItem('token');
+  if (token) {
+    $('#g-signin').hide()
+    $('#g-signout').show()
+    $('#content').show()
+  } else {
+    $('#g-signin').show()
+    $('#g-signout').hide()
+    $('#content').hide()
+  }
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+    localStorage.clear();
+    isLogin();
+  });
+}
